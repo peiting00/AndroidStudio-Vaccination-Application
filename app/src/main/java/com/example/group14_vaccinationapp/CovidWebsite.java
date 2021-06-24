76a6905a5b14ce7143ffa0a4d42fdb4e75c7881c
Peiting Chew <i19017904@student.newinti.edu.my>
2021-06-24 10:09:15 +0800

    CovidWebsite

--- app/src/main/java/com/example/group14_vaccinationapp/CovidWebsite.java
+++ app/src/main/java/com/example/group14_vaccinationapp/CovidWebsite.java
@@ -1,14 +1,21 @@
 package com.example.group14_vaccinationapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class CovidWebsite extends AppCompatActivity {
 
     Toolbar toolbar;
+    private TextView mWebsite1,mWebsite2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,8 @@ public class CovidWebsite extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mWebsite1=findViewById(R.id.web_desc);
+        mWebsite2=findViewById(R.id.web_desc_2);
     }
 
     @Override //when back button clicked
@@ -29,5 +38,27 @@ public class CovidWebsite extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openWebsite(View view) {
+        if (view.getId() == R.id.web_desc) {
+            String url = mWebsite1.getText().toString();
+            Uri webpage = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Log.d("Website:JKJAV", "Can't handle this!");
+            }
+        } else if (view.getId() == R.id.web_desc_2) {
+            String url = mWebsite2.getText().toString();
+            Uri webpage = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Log.d("Website:MOH", "Can't handle this!");
+            }
+        }
     }
 }
