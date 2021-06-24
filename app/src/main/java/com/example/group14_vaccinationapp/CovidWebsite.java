7cbcfe46a6a2063d7b8a58023c6d89ed372b3362
Peiting Chew <i19017904@student.newinti.edu.my>
2021-06-24 11:44:47 +0800

    done websites

--- app/src/main/java/com/example/group14_vaccinationapp/CovidWebsite.java
+++ app/src/main/java/com/example/group14_vaccinationapp/CovidWebsite.java
@@ -2,6 +2,7 @@ package com.example.group14_vaccinationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -40,25 +41,26 @@ public class CovidWebsite extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void openWebsite(View view) {
-        if (view.getId() == R.id.web_desc) {
-            String url = mWebsite1.getText().toString();
-            Uri webpage = Uri.parse(url);
-            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            } else {
-                Log.d("Website:JKJAV", "Can't handle this!");
-            }
-        } else if (view.getId() == R.id.web_desc_2) {
-            String url = mWebsite2.getText().toString();
-            Uri webpage = Uri.parse(url);
-            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            } else {
-                Log.d("Website:MOH", "Can't handle this!");
-            }
+
+    public void openWebsiteJKJAV(View view) {
+        mWebsite1=findViewById(R.id.web_address);
+        String url = mWebsite1.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(SearchManager.QUERY,url); // url is the query
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
         }
+
+    }
+
+    public void openWebsiteMOH(View view) {
+        mWebsite2=findViewById(R.id.web_address);
+        String url2 = mWebsite2.getText().toString();
+        Intent intent2 = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent2.putExtra(SearchManager.QUERY,url2); // url is the query
+        if(intent2.resolveActivity(getPackageManager())!=null){
+            startActivity(intent2);
+        }
+
     }
 }
